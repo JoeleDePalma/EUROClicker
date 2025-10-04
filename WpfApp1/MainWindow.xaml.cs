ea9d3ec30e8afc434a1367d85ae718f89b810327
Joele De Palma <joeledepalma135@gmail.com>
2025-10-04 17:07:58 +0200

    Aggiunta applicazione WPF "Clicker" in .NET 8.0
    Sono stati creati i file necessari per un'applicazione WPF chiamata "Clicker".
    Include `App.xaml` e `App.xaml.cs` per la logica dell'applicazione,
    `AssemblyInfo.cs` per le informazioni sul tema, e `Clicker.csproj`
    per le proprietà del progetto. È stata implementata la finestra principale
    `MainWindow.xaml` con un'interfaccia utente che consente di visualizzare
    il conteggio e gestire gli upgrade del valore per clic tramite i pulsanti
    nella finestra principale.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public long EUValue;
+        public long EUClick;
+        public long PriceUpgradeEUClick;
+        public long ClickUpgradeValue;
+
+        public MainWindow()
+        {
+            this.EUValue = 0;
+            this.EUClick = 1;
+            this.PriceUpgradeEUClick = 10;
+            this.ClickUpgradeValue = 1;
+
+            InitializeComponent();
+
+            CountTextBlock.Text = EUValue.ToString();
+            UpgradeButton.Content = $"+{ClickUpgradeValue.ToString()}€ per click\n-{PriceUpgradeEUClick.ToString()}€";
+
+        }
+
+        private void UpgradeClickValue(object sender, RoutedEventArgs e)
+        {
+            EUValue += EUClick;
+            CountTextBlock.Text = EUValue.ToString();
+        }
+
+        private void UpgradeEUPerClick(object sender, RoutedEventArgs e)
+        {
+
+            if (EUValue < PriceUpgradeEUClick) return;
+            EUClick += ClickUpgradeValue;
+            EUValue -= PriceUpgradeEUClick;
+            ClickUpgradeValue += 1;
+            PriceUpgradeEUClick += 50;
+            UpgradeButton.Content = $"+{ClickUpgradeValue.ToString()}€ per click\n-{PriceUpgradeEUClick.ToString()}€";
+            CountTextBlock.Text = EUValue.ToString();
+
+        }
+    }
+}
